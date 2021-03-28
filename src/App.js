c9923ed0f13cbaffd80f663362c6e758cd44c99d
RoseKimball <mrkimbal@usc.edu>
2021-03-27 17:45:48 -0700

    init

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import './App.scss';
 
-function App() {
+const App = () => {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      
     </div>
   );
 }
 
 export default App;
+
+/*
+
+container
+  header
+    logo
+    menu
+    tagline
+  description
+    interactive image
+    header + copy
+  creations
+    header
+    see all button
+    grid (generate dynamically with images + titles)
+      grid components
+  footer
+    logo
+    links
+    socials
+    all rights reserved
+
+*/
